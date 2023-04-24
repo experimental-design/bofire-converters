be29c6847699637de2786fea4d15194958ed6910
Robert Lee <robert-matthew.lee@basf.com>
2023-04-24 13:57:47 +0200

    convert inputs function started

--- opti_to_domain.py
+++ opti_to_domain.py
@@ -3,14 +3,55 @@ from bofire.data_models.constraints.api import (
     LinearInequalityConstraint,
 )
 from bofire.data_models.domain.api import Domain
-from bofire.data_models.features.api import ContinuousInput, ContinuousOutput
+from bofire.data_models.features.api import (
+    CategoricalInput,
+    ContinuousInput,
+    ContinuousOutput,
+    DiscreteInput,
+)
+from opti import Categorical, Continuous, Discrete, Parameters
+
+# Note: Parameter types are Continuous, Discrete Categorical from opti
+# Domain input_features can be ?
 
 
 def convert_inputs():
-    pass
+
+    # opti inputs example:
+    inputs = Parameters(
+        [
+            Discrete("x1", domain=[0.0, 1.0, 2.0, 3.0]),
+            Continuous("x2", domain=[-2.0, 2.0]),
+            Continuous("x3", domain=[-2.0, 2.0]),
+            Continuous("x4", domain=[-2.0, 2.0]),
+        ]
+    )
+
+    convert_types = (
+        {
+            "discrete": DiscreteInput,
+            "continuous": ContinuousInput,
+            "categorical": CategoricalInput,
+        },
+    )
+    # convert_types_bounds={'discrete': values,'continuous': bounds,'categorical':categories},
+
+    d_inputs = []
+    for key, value in inputs.parameters.items():
+        if value.type == "categorical":
+            d_type = convert_types[value.type](key=key, categories=value.domain)
+        elif value.type == "continuous":
+            d_type = "somthinig else"
+
+    tmp = Categorical
+    # d_input=ContinuousInput(key="x1", bounds=(0, 1))
+    d_type
+    tmp
+
+    return d_inputs
 
 
-def convert_iutputs_and_objectives():
+def convert_intputs_and_objectives():
     pass
 
 
