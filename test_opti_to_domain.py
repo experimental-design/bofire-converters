cd163fcb2792e0e64d4e8faef4e5153973a75662
Robert Lee <robert-matthew.lee@basf.com>
2023-04-24 14:28:47 +0200

    use pytest parametrization

--- test_opti_to_domain.py
+++ test_opti_to_domain.py
@@ -1,3 +1,4 @@
+import pytest
 from bofire.data_models.domain.api import Domain
 from opti.problems.cbo_benchmarks import (
     G4,
@@ -18,26 +19,24 @@ from opti.problems.mixed import DiscreteFuelInjector, DiscreteVLMOP2
 from opti_to_domain import domain_from_opti
 
 test_problems = [
-    G4,
-    G6,
-    G7,
-    G8,
-    G10,
-    Gardner,
-    Gramacy,
-    PressureVessel,
-    Sasena,
-    SpeedReducer,
-    TensionCompression,
-    WeldedBeam1,
-    DiscreteFuelInjector,
-    DiscreteVLMOP2,
+    G4(),
+    G6(),
+    G7(),
+    G8(),
+    G10(),
+    Gardner(),
+    Gramacy(),
+    PressureVessel(),
+    Sasena(),
+    SpeedReducer(),
+    TensionCompression(),
+    WeldedBeam1(),
+    DiscreteFuelInjector(),
+    DiscreteVLMOP2(),
 ]
 
 
-def test_convert_benchmarks():
-    # here
-    for test_problem_class in test_problems:
-        test_problem = test_problem_class()
-        test_domain = domain_from_opti(test_problem)
-        assert isinstance(test_domain, Domain)
+@pytest.mark.parametrize("test_problem", test_problems)
+def test_convert_benchmarks(test_problem):
+    bofire_domain = domain_from_opti(test_problem)
+    assert isinstance(bofire_domain, Domain)
