5ac1bee2a5d5b68a3fee5bb8e7a25c3c672d6ed9
Robert Lee <84771576+R-M-Lee@users.noreply.github.com>
2023-07-04 12:22:42 +0200

    cleanup for pypi version

--- domainconverters/opti_to_domain.py
+++ domainconverters/opti_to_domain.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 from warnings import warn
 
 from bofire.data_models.constraints.api import (
+    Constraint,
     LinearEqualityConstraint,
     LinearInequalityConstraint,
     NChooseKConstraint,
@@ -15,6 +16,7 @@ from bofire.data_models.features.api import (
     ContinuousInput,
     ContinuousOutput,
     DiscreteInput,
+    Output,
 )
 from bofire.data_models.objectives.api import (
     CloseToTargetObjective,
@@ -22,10 +24,7 @@ from bofire.data_models.objectives.api import (
     MinimizeObjective,
 )
 from opti import (
-    Categorical,
     CloseToTarget,
-    Continuous,
-    Discrete,
     Maximize,
     Minimize,
     Objectives,
@@ -41,12 +40,9 @@ from opti.constraint import (
     NonlinearInequality,
 )
 
-# Note: Parameter types are Continuous, Discrete Categorical from opti
-# Domain input_features can be ?
-
 
 def convert_inputs(inputs: Parameters) -> List:
-    """Make the Bofire equivalent to a Parameters objects from (m)opti
+    """Make the Bofire equivalent to an inputs objects from (m)opti
 
     Parameters:
         inputs: The inputs from an opti problem generated using
@@ -74,8 +70,6 @@ def convert_inputs(inputs: Parameters) -> List:
         "categorical": {"type": CategoricalInput, "domain": "categories"},
     }
 
-    # convert_types_bounds={'discrete': values,'continuous': bounds,'categorical':categories},
-
     d_inputs = []
     for key, value in inputs.parameters.items():
         kwargs = {"key": key, convert_types[value.type]["domain"]: value.domain}
@@ -88,7 +82,7 @@ def convert_outputs_and_objectives(
     outputs: Parameters,
     objectives: Objectives,
     output_constraints: Optional[Objectives] = None,
-) -> List:
+) -> List[Output]:
     """Make BoFire outputs from opti outputs and objectives
 
     opti specifies experimental outputs, which are quantities to be observed
@@ -150,9 +144,6 @@ def convert_outputs_and_objectives(
                 warn(f"{out} has been converted to a continuous output.")
                 bof_out = ContinuousOutput(key=f"{out}{suffix}", objective=bof_obj)
             elif outputs[out].type == "categorical":
-                warn(
-                    f"Categorical output {out} has been entered into an objective with equal weights assigned to each category."
-                )
                 bof_out = CategoricalOutput(
                     key=f"{out}{suffix}",
                     type="CategoricalOutput",
@@ -162,17 +153,15 @@ def convert_outputs_and_objectives(
                     ],
                 )
             else:
-                raise Exception("Unhandled output type: {outputs[out].type}")
+                raise Exception(f"Unhandled output type: {outputs[out].type}")
 
             output_list.append(bof_out)
 
     return output_list
 
 
-def convert_constraints(opti_constraints: Constraints) -> List:
+def convert_constraints(opti_constraints: Constraints) -> List[Constraint]:
     """opti constraints to bofire constraints
-
-    Just straightforward renaming here.
 
     Parameters:
         opti_constraints: The constraints to be converted
@@ -259,35 +248,3 @@ def convert_problem(opti_problem: Problem) -> Domain:
     )
 
     return bofire_domain
-
-
-if __name__ == "__main__":
-    # inputs = Parameters(
-    #     [
-    #         Discrete("x1", domain=[0.0, 1.0, 2.0, 3.0]),
-    #         Continuous("x2", domain=[-2.0, 2.0]),
-    #         Continuous("x3", domain=[-2.0, 2.0]),
-    #         Continuous("x4", domain=[-2.0, 2.0]),
-    #         Categorical("x5", domain=["cat", "dog", "monkey"]),
-    #     ]
-    # )
-
-    # print(convert_inputs(inputs))
-    outputs = Parameters(
-        [
-            Discrete("meetings", domain=[0.0, 1.0, 2.0, 3.0]),
-            Continuous("coffee", domain=[0, 20.0]),
-            Continuous("seriousness", domain=[0, 10.0]),
-            Categorical("animal", domain=["cat", "dog", "monkey"]),
-        ]
-    )
-
-    objectives = Objectives(
-        [
-            Minimize("meetings", target=2),
-            Maximize("coffee", target=4),
-            CloseToTarget("seriousness", target=5, exponent=2, tolerance=1.1),
-        ]
-    )
-
-    out_list = convert_outputs_and_objectives(outputs, objectives)
